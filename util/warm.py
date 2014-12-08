8f3fc9c763071ef201f4510372aaf2ae5edd632b
John Krauss <irving.krauss@gmail.com>
2014-12-08 16:47:49 -0500

    allow customization of proxy location

--- util/warm.py
+++ util/warm.py
@@ -1,20 +1,31 @@
 import json
 import time
+import sys
 
 from urllib2 import urlopen, Request
 
 if __name__ == '__main__':
+  try:
+    proxy = sys.argv[1]
+  except:
+    print '''ERROR you must specify proxy to warm as first argument:
+
+python util/warm.py 'http://www.opendatacache.com'
+'''
+    sys.exit(1)
+  print u'Warming {}'.format(proxy)
+
   with open('supported_portals.txt') as f:
     for host in f:
       host = host.strip()
-      idx_resp = urlopen(Request('http://localhost:5003/' + host + '/data.json'))
+      idx_resp = urlopen(Request(proxy + host + '/data.json'))
       for dataset in json.loads(idx_resp.read()):
         identifier = dataset['identifier']
 
         if identifier in ('data.json'):
           continue
 
-        url = 'http://localhost:5003/' + host + '/data/' + identifier
+        url = proxy + host + '/data/' + identifier
 
         req = Request(url)
         req.add_header('Accept-Encoding', 'gzip, deflate')
