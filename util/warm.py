8df536eefb9e77539cd6ccb8f445376546cddc45
John Krauss <irving.krauss@gmail.com>
2014-12-08 18:51:15 -0500

    error handling, better logging

--- util/warm.py
+++ util/warm.py
@@ -5,8 +5,9 @@ Warm the cache.
 import json
 import time
 import sys
+from datetime import datetime
 from urlparse import urljoin
-from urllib2 import urlopen, Request
+from urllib2 import urlopen, Request, HTTPError
 
 if __name__ == '__main__':
     try:
@@ -18,7 +19,7 @@ if __name__ == '__main__':
 python util/warm.py 'http://www.opendatacache.com/'
 ''')
         sys.exit(1)
-    sys.stdout.write(u'Warming {}\n'.format(PROXY))
+    sys.stdout.write(u'{}\t{}\t{}\n'.format('warm', PROXY, datetime.now()))
     sys.stdout.flush()
 
     with open('supported_portals.txt') as f:
@@ -36,19 +37,31 @@ python util/warm.py 'http://www.opendatacache.com/'
                 req = Request(url)
                 req.add_header('Accept-Encoding', 'gzip, deflate')
 
-                sys.stdout.write(u'Requesting {}\n'.format(url))
                 start = time.time()
-                resp = urlopen(req)
-                end = time.time()
-                sys.stdout.write(u'time to request {}: {}\n'.format(
-                    url, end - start))
-                sys.stdout.flush()
 
-                sys.stdout.write(u'Reading {}\n'.format(url))
-                start = time.time()
-                respdata = resp.read()
-                end = time.time()
-                sys.stdout.write(
-                    u'time to download {} bytes from {}: {}\n'.format(
-                        len(respdata), url, end - start))
+                try:
+                    resp = urlopen(req)
+                except HTTPError as err:
+                    end = time.time()
+                    sys.stdout.write(u'{}\t{}\t{}\t{}\t\n'.format(
+                        'request', err, url, end - start))
+                    sys.stderr.flush()
+                else:
+                    end = time.time()
+                    sys.stdout.write(u'{}\t{}\t{}\t{}\t\n'.format(
+                        'request', 'success', url, end - start))
+                    sys.stdout.flush()
+
+                    start = time.time()
+                    try:
+                        respdata = resp.read()
+                    except HTTPError as err:
+                        end = time.time()
+                        sys.stdout.write(u'{}\t{}\t{}\t{}\t\n'.format(
+                            'content', err, url, end - start))
+                    else:
+                        end = time.time()
+                        sys.stdout.write(u'{}\t{}\t{}\t{}\t{}\n'.format(
+                            'content', 'success', url, end - start,
+                            len(respdata)))
                 sys.stdout.flush()
