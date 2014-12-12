0247acbabb32d151818b8105557acccf7fba9d9d
John Krauss <irving.krauss@gmail.com>
2014-12-12 17:16:44 -0500

    fix bug preventing list of ids

--- util/warm.sh
+++ util/warm.sh
@@ -42,6 +42,7 @@ function warm_portal {
   # actually parsing the json.
   now=$(date +"%Y-%m-%dT%H:%M:%S%z")
   ids=$(curl -s -S -w "$ow" $url | grep -Po '"identifier":(.*?[^\\])",' | cut -b 15-23)
+  mkdir -p logs/$portal
   printf "$ids\n" > logs/$portal/ids-$now.log
   for id in $ids
   do
