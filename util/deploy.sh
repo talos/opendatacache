f0a59d42d61627ebe7b14410ea72f776d0667f07
John Krauss <irving.krauss@gmail.com>
2015-01-08 13:48:55 -0500

    forbid changes to site during deploy, use persistent storage

--- util/deploy.sh
+++ util/deploy.sh
@@ -6,7 +6,6 @@ docker rm opendatacache
 docker pull thegovlab/opendatacache:latest
 export WARM_URL="http://www.opendatacache.com"
 docker run -v $(pwd)/cache:/cache \
-  -v $(pwd)/site:/opendatacache/site \
   -v $(pwd)/logs:/var/log/opendatacache \
   -e "WARM_URL=$WARM_URL" \
   -d -i -p 80:8081 --name=opendatacache thegovlab/opendatacache:latest
