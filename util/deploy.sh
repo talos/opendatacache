249c35a5b9458cd0aecc3ff422712771cd1cf025
John Krauss <irving.krauss@gmail.com>
2015-01-09 16:43:46 -0500

    don't share logs directory

--- util/deploy.sh
+++ util/deploy.sh
@@ -6,6 +6,5 @@ docker rm opendatacache
 docker pull thegovlab/opendatacache:latest
 export WARM_URL="http://www.opendatacache.com"
 docker run \
-  -v $(pwd)/logs:/var/log/opendatacache \
   -e "WARM_URL=$WARM_URL" \
   -d -i -p 80:8081 --name=opendatacache thegovlab/opendatacache:latest
