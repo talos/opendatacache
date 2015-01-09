65c7f5c6497b5811a40b090167b8465c7485b112
John Krauss <irving.krauss@gmail.com>
2015-01-09 16:14:36 -0500

    actually, use cache internal to container

--- util/deploy.sh
+++ util/deploy.sh
@@ -3,11 +3,9 @@
 docker stop opendatacache
 docker rm opendatacache
 
-rm -rf cache
-
 docker pull thegovlab/opendatacache:latest
 export WARM_URL="http://www.opendatacache.com"
-docker run -v $(pwd)/cache:/cache \
+docker run \
   -v $(pwd)/logs:/var/log/opendatacache \
   -e "WARM_URL=$WARM_URL" \
   -d -i -p 80:8081 --name=opendatacache thegovlab/opendatacache:latest
