8eeeffbe23dc0e5ada4582436f64c0328b2bc6e9
talos <irving.krauss@gmail.com>
2015-03-07 11:01:54 -0500

    fixes to escaping

--- util/warm.sh
+++ util/warm.sh
@@ -71,15 +71,15 @@ function warm_portal {
       sizes=$(wget --header='Accept-Encoding: gzip' --progress=dot --no-check-certificate -O - "$url" 2>${wgetlog} | gunzip | wc | sed -r 's/ +/	/g' | tr -d '\n')
       url_effective=$(cat ${wgetlog} | head -n 1 | cut -d ' ' -f 4)
       http_code=$(cat ${wgetlog} | grep -m 1 HTTP | grep -Po '\d{3}')
-      size_download=$(cat ${wgetlog} | tail -n 2 | cut -f 2 -d '[' | cut -f 1 -d ']')
+      size_download=$(cat ${wgetlog} | tail -n 2 | cut -f 2 -d '[' | cut -f 1 -d ']' | cut -f 1 -d '/')
       speed_download=$(cat ${wgetlog} | tail -n 2 | cut -f 2 -d '(' | cut -f 1 -d ')')
       output="${http_code}	${id}	${now}	${size_download}	${speed_download}	${url_effective}"
 
       metadata=$(curl -k -s -S --compressed "${metadata_url}" | tee $logs/api/views/$id/meta.json)
       columns="name attribution averageRating category createdAt description displayType downloadType downloadCount newBackend numberOfComments oid rowsUpdatedAt rowsUpdatedBy tableId totalTimesRated viewCount viewLastModified viewType tags"
       for key in $columns; do
-        val=$(echo "$metadata" | grep "\"$key\" :" | head -n 1 | grep -Po ': .*' | sed -r 's/^: "?//' | sed -r 's/"?,$//' | sed -r 's/(\\r|\\n|\\t)//g')
-        output="$output\t$val"
+        val=$(printf "$metadata" | grep "\"$key\" :" | head -n 1 | grep -Po ': .*' | tr -cd '[:print:]' | sed -r 's/^: "?//' | sed -r 's/"?,$//' | tr -s '[:space:]' ' ')
+        output="$output	$val"
       done
       output="${output}${sizes}"
 
@@ -88,7 +88,7 @@ function warm_portal {
         continue
       fi
 
-      printf "$output\n" | tee -a $logs/api/views/$id/index.log
+      echo "$output" | tee -a $logs/api/views/$id/index.log
       tail -n 1 -q $logs/api/views/**/index.log > $logs/summary.log
       cat $logroot/**/status.log > $logroot/status.log
 
