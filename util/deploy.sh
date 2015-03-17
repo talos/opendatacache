c73bdaea8fca65f5f668784f7e6a912159f48d2f
John Krauss <irving.krauss@gmail.com>
2015-03-17 13:28:02 -0400

    bypass DNS entirely for caching

--- util/deploy.sh
+++ util/deploy.sh
@@ -4,7 +4,7 @@ docker stop opendatacache
 docker rm opendatacache
 
 docker pull thegovlab/opendatacache:latest
-export WARM_URL="http://www.opendatacache.com"
+export WARM_URL="http://localhost:8081"
 docker run \
   -e "WARM_URL=$WARM_URL" \
   -d -i -p 80:8081 --name=opendatacache thegovlab/opendatacache:latest
